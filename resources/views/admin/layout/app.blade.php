d964bf00e448f70ff422c1a838290467c80eb9c3
nasywa-raichanah <nasywaraichanah15.2@gmail.com>
2025-05-30 17:07:00 +0700

    add gambar, dll

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -11,9 +11,9 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-  <link rel="icon" type="image/png" href="{{ asset('style/TheEvent/assets/img/logo.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('style/TheEvent/assets/img/logo_icon.png') }}">
   <title class="text-uppercase">
-    SMC XII - {{ $title }}
+    SMO II - {{ $title }}
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet" />
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -3,7 +3,7 @@
 <div class="sidenav-header">
     <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-0" href="{{ route('home') }}" target="_blank">
-    <img src="{{ asset('style/TheEvent/assets/img/logo.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+    <img src="{{ asset('style/TheEvent/assets/img/logo_panjang.png') }}" class="navbar-brand-img h-100" alt="main_logo">
     {{-- <img class="w-50 mx-auto" src="../assets/img/illustrations/icon-documentation.svg" alt="sidebar_illustration"> --}}
 
     {{-- <span class="ms-1 font-weight-bold">Argon Dashboard 2</span> --}}
